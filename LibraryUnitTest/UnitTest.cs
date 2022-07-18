c8c6a7facb4c8f4b12852cf3e328cdea6e25aec6
AlexHomePC <gotlib14@gmail.com>
2022-07-18 21:24:23 +0300

    Added Unit test Librery Repos

--- LibraryUnitTest/UnitTest.cs
+++ LibraryUnitTest/UnitTest.cs
@@ -10,16 +10,43 @@ namespace LibraryUnitTest
     [TestClass]
     public class UnitTest1
     {
-        #region ISBN Unit Test
+        #region Library Repositor Unit Test
         readonly LbraryRepository lb = new LbraryRepository();
+        readonly DataMock dm = new DataMock();
+        Book b1 = new Book("", DateTime.Now, "adwq");
 
         [TestMethod]
-        public void GetLibraryItemsList()
+        public void GetLibraryItem()
         {
             Assert.IsTrue(lb.Get(Guid.Empty) == null);
         }
 
+        [TestMethod]
+        public void GetLibraryItemsList()
+        {
+            Assert.IsTrue(lb.Get() == dm.LibraryItemsList);
+        }
 
+        [TestMethod]
+        public void AddLibraryItemsToList()
+        {
+            
+            Assert.IsNotNull(lb.Add(b1));
+        }
+
+        [TestMethod]
+        public void GetLibreryItemById()
+        {
+            Guid id = b1.Id;
+            Assert.IsTrue(lb.Get(id) == b1);
+        }
+
+        [TestMethod]
+        public void UpddateLibraryItem()
+        {
+            b1.Title = "1111";
+            Assert.IsTrue(lb.Update(b1) == b1);
+        }
 
         #endregion
     }
